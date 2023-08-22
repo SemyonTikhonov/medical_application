9fce1f6ee67d4c5873e13fc66f28ae31ba31109d
SemyonTikhonov <davidpaley638@gmail.com>
2023-08-22 14:40:28 +0300

    1. Added `import 'package:flutter/material.dart';`: This line imports the necessary package to use Flutter's material design widgets, including `AppBar` and `Scaffold`.
    2. Updated `StatefulWidget` inheritance: The `HomeScreen` class is now a subclass of `StatefulWidget` and it includes a `const` constructor that takes a `Key` argument.
    
    3. Added a `key` parameter to `Scaffold`: The `key` parameter is used to uniquely identify the widget for performance optimization and widget reuse. It is optional, but considered a good practice to include it when using `Scaffold`.
    
    4. Added a `const` modifier to `Text` widget: The `Text('Товары')` widget has been marked as `const`, which means that its properties won't change during runtime. This can provide slight performance benefits.
    
    5. Updated `AppBar` title: The text "Товары" has been provided as the title of the `AppBar`.
    
    6. Updated `IconButton` icons: The icons for the `IconButton` widgets are now `Icon(Icons.notifications)` for the notifications button and `Icon(Icons.shopping_cart)` for the cart button.
    
    7. Updated the body text: The `Text('AllStom')` widget has been replaced with `Text('AllStom')` as the centered text in the body of the `Scaffold`.

---
+++ lib/src/home/home_screen1.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/logo.png', // Путь к логотипу приложения
+              height: 30, // Высота логотипа
+            ),
+            const SizedBox(width: 8), // Отступ между логотипом и текстом
+            const Text(
+              'Товары',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.notifications),
+            onPressed: () {
+              // Действие при нажатии на иконку уведомлений
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Действие при нажатии на иконку корзины
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('AllStom'),
+      ),
+    );
+  }
+}
