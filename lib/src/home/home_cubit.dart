8101d8ad44ce37ec84723b30f0f7552b5b0967eb
SemyonTikhonov <davidpaley638@gmail.com>
2023-08-27 00:19:49 +0300

    Created Cubit for the home_screen.dart.

--- lib/src/home/home_cubit.dart
+++ lib/src/home/home_cubit.dart
@@ -1,2 +1,25 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+
+class HomeScreenState extends Equatable {
+  final List<String> products;
+
+  const HomeScreenState({required this.products});
+
+  HomeScreenState copyWith({List<String>? products}) {
+    return HomeScreenState(products: products ?? this.products);
+  }
+
+  @override
+  List<Object> get props => [products];
+}
+
+// Создаем Cubit для экрана HomeScreen
+class HomeScreenCubit extends Cubit<HomeScreenState> {
+  HomeScreenCubit() : super(const HomeScreenState(products: []));
+
+  // Определяем метод для обновления списка товаров
+  void updateProducts(List<String> newProducts) {
+    emit(state.copyWith(products: newProducts));
+  }
+}
